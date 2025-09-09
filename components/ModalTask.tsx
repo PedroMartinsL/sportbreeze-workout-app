d479ab500a8f440600b49849bbaefd5d35bded45
Pedro Martins <pedromartinsdelemos@gmail.com>
2025-09-08 22:42:26 -0300

    AlertModal - warn notification, gradient bib

--- components/ModalTask.tsx
+++ components/ModalTask.tsx
@@ -1,104 +1,114 @@
- import { View, Text, Modal, Pressable } from "react-native";
+import { View, Text, Modal, Pressable, ScrollView } from "react-native";
 import { DetachedData, TaskCardProps } from "./TaskCard";
 import { WeatherIcon } from "./WeatherIcon";
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 interface TaskProps {
-    task: TaskCardProps;
-    modalVisible: boolean,
-    setModalVisible: Function,
+  task: TaskCardProps;
+  modalVisible: boolean;
+  setModalVisible: Function;
 }
 
-export default function ModalTask({ task, modalVisible, setModalVisible }: TaskProps) {
-
-    return (
-        <Modal
-            animationType="slide"
-            transparent={true}
-            visible={modalVisible}
-            onRequestClose={() => setModalVisible(false)}
-        >
-            {/* Fundo semitransparente */}
-            <View className="flex-1 bg-opacity-50"
-            >
-                
-                <View className="bg-white w-full h-full rounded-t-xl p-6 shadow-lg">
-
-                    {/* Botão de fechar no canto */}
-                    <View className="flex-row justify-end">
-                        <Pressable onPress={() => setModalVisible(false)}>
-                            <MaterialIcons name="cancel" size={28} color="black" />
-                        </Pressable>
-                    </View>
-                    
-                    {/* Conteúdo centralizado */}
-                    <View className="mt-10 gap-10">
-                         <View className="flex-row justify-around w-full items-end">
-                            <Text className="text-6xl font-bold text-gray-800">{task.sport}</Text>
-
-                            <Text className="text-lg text-gray-800">{task.hour}</Text>
-                        </View>
-
-                        
-
-                        <View className="flex-row justify-between w-full px-2">
-                            <View className="w-full">
-
-                            
-                            <DetachedData>
-                                Duration:
-                            </DetachedData>
-                            
-                            <Text className="text-lg font-light ml-5 mt-5">{task.duration} min</Text>
-                            </View>
-                            <View>
-                
-                            </View>
-                        </View>
-
-                        <View className="border-b border-gray-200 mb-4" />
-
-                        <View className="flex-row justify-between w-full px-2 mt-10">
-                            <DetachedData>
-                                Thoughtput:
-                            </DetachedData>
-                            
-                            <Text className="text-lg font-light">{task.kcal} kcal</Text>
-
-                             <DetachedData>
-                                Temperature:
-                            </DetachedData>
-
-                            <Text className="text-lg font-light">{task.temp}°C</Text>
-                        </View>
-                        <View className="border-b border-gray-200 mb-4" />
-
-                        <View className="justify-center items-center">
-
-                            <View className="border rounded-md p-4 border-1 border-gray-500">
-
-                            <Text className="text-lg font-light">{task.routine}</Text>
-                            </View>
-                        </View>
-
-                        
-                        
-                        <View className="flex-row justify-around">
-                            <View>
-                                <Text className="text-lg font-medium">
-                                Planner:
-                                </Text>
-                                <Text className="text-lg font-light">{task.planner}</Text>
-                            </View>
-
-                            <WeatherIcon weather={task.weather} size={100} />
-                        </View>
-                        
-                              
-                    </View>
-                </View>
+export default function ModalTask({
+  task,
+  modalVisible,
+  setModalVisible,
+}: TaskProps) {
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={() => setModalVisible(false)}
+    >
+      {/* Fundo semitransparente */}
+      <ScrollView>
+        <View className="flex-1 bg-opacity-50">
+          <View className="bg-white w-full h-full rounded-t-xl p-6 shadow-lg">
+            {/* Botão de fechar no canto */}
+            <View className="flex-row justify-end">
+              <Pressable onPress={() => setModalVisible(false)}>
+                <MaterialIcons name="cancel" size={28} color="black" />
+              </Pressable>
             </View>
-        </Modal>
-    );
+
+            {/* Conteúdo centralizado */}
+            <View className="mt-10 gap-10">
+              <View className="flex-row justify-around w-full items-end">
+                <Text className="text-6xl font-bold text-gray-800">
+                  {task.sport}
+                </Text>
+
+                <Text className="text-lg text-gray-800">{task.hour}</Text>
+              </View>
+
+              <View className="justify-center items-center">
+                <View className="border-b p-1 border-t border-gray-500">
+                  <Text className="text-lg font-light">{task.routine}</Text>
+                </View>
+              </View>
+
+              <View className="flex-row justify-around">
+                <View>
+                  <Text className="text-lg font-medium">Planner:</Text>
+                  <Text className="text-lg font-light w-40">
+                    {task.planner}
+                  </Text>
+                </View>
+
+                <WeatherIcon weather={task.weather} size={100} />
+              </View>
+
+              <View className="flex-row justify-between w-full px-2">
+                <View className="w-full">
+                  <DetachedData>Duration:</DetachedData>
+
+                  <Text className="text-lg font-light ml-5 mt-5">
+                    {task.duration} min
+                  </Text>
+                </View>
+                <View></View>
+              </View>
+
+              <View className="border-b border-gray-200 mb-4" />
+
+              <View className="w-full px-2 h-full">
+                <View className="flex-row justify-between items-center mb-10">
+                  {/* Coluna 1 */}
+                  <View className="flex-1 mr-2">
+                    <DetachedData>Thoughtput:</DetachedData>
+                    <Text className="text-lg font-light ml-5 mt-5">
+                      {task.kcal} kcal
+                    </Text>
+                  </View>
+
+                  {/* Coluna 2 */}
+                  <View className="flex-1 ml-2">
+                    <DetachedData>Temperature:</DetachedData>
+                    <Text className="text-lg font-light ml-5 mt-5">
+                      {task.temp}°C
+                    </Text>
+                  </View>
+                </View>
+
+                <View className="border-b border-gray-200" />
+
+                <View className="flex-row justify-between w-full px-4 mt-4">
+                  {/* Atualizar */}
+                  <Pressable className="items-center">
+                    <MaterialIcons name="edit" size={28} color="black" />
+                  </Pressable>
+
+                  {/* Deletar */}
+                  <Pressable className="items-center">
+                    <MaterialIcons name="delete" size={28} color="black" />
+                  </Pressable>
+                </View>
+              </View>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </Modal>
+  );
 }
--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
-import { View, Text, Switch } from "react-native";
+import { View, Text, Switch, Pressable, StyleSheet } from "react-native";
 import { WeatherIcon } from "./WeatherIcon";
-
+import AlertModal from "./AlertModal";
+import { LinearGradient } from 'expo-linear-gradient';
 
 export type TaskCardProps = {
-  id: number,
-  day: number,
-  weather: string,
-  kcal: number,
-  routine: string,
-  temp: number,
-  duration: number,
-  planner: string,
-  hour: string,
-  date: string,
-  sport: string
+  id: number;
+  day: number;
+  weather: string;
+  kcal: number;
+  routine: string;
+  temp: number;
+  duration: number;
+  planner: string;
+  hour: string;
+  date: string;
+  sport: string;
 };
 
 type DetachedDataProps = {
@@ -30,73 +31,79 @@ export function DetachedData({ children }: DetachedDataProps) {
 }
 
 export default function TaskCard(props: TaskCardProps) {
-
   const [isEnabled, setIsEnabled] = useState(false);
+  const [warnModalVisible, setWarnModalVisible] = useState(false);
 
-  const toggleSwitch = () => setIsEnabled(previous => !previous);
+  const toggleSwitch = () => setIsEnabled((previous) => !previous);
 
-  const warn_days = [
-    "frosty",
-    "rainy",
-    "thundering",
-  ]
+  const warn_days = ["frosty", "rainy", "thundering"];
 
   const warn = warn_days.includes(props.weather);
 
   return (
-    <View
-    className="bg-white rounded-xl p-6 mb-4 w-full max-w-md shadow-lg mt-4"
-    style={{
-      elevation: 5,
-      shadowColor: "#000",
-      shadowOffset: { width: 0, height: 2 },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.84,
-    }}
-  >
-    {/* Top row: date/hour + warn */}
-    <View className="flex-row justify-between items-center mb-3">
-      <View className="flex-row gap-3">
-        <DetachedData>{props.date}</DetachedData>
-        <DetachedData>{props.hour}</DetachedData>
-      </View>
-      {warn && (
-        <View className="bg-red-600 rounded-2xl py-1 px-3">
-          <Text className="text-white font-bold text-sm">Warn</Text>
+    <LinearGradient
+      colors={warn ? ['#564770', '#FFFFFF'] : ['#FFFFFF', '#FFFFFF']} // vermelho intenso para amarelo
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 0 }}
+      className="p-6 mb-4 w-full max-w-md mt-4 shadow-lg"
+      style={styles.componentCard}
+    >
+      {/* Top row: date/hour + warn */}
+      <View className="flex-row justify-between items-center mb-3">
+        <View className="flex-row gap-3">
+          <DetachedData>{props.date}</DetachedData>
+          <DetachedData>{props.hour}</DetachedData>
         </View>
-      )}
-    </View>
+        {warn && (
+          <View className="bg-red-600 rounded-2xl py-1 px-3">
+            <Pressable onPress={() => setWarnModalVisible(true)}>
 
-    {/* Divider */}
-    <View className="border-b border-gray-200 mb-5" />
-
-    {/* Bottom row: WeatherIcon + info + switch */}
-    <View className="flex-row items-center justify-between gap-6 my-10">
-      <View className="flex-row">
-
-      <View className="flex-row gap-5">
-        <View className="bg-gray-300 rounded-full px-4 py-2">
-        <Text className="text-lg font-bold">{props.temp}°C</Text>
+            <Text className="text-white font-bold text-sm">Warn</Text>
+            </Pressable>
+            <AlertModal
+              visible={warnModalVisible}
+              onClose={() => setWarnModalVisible(false)}
+              title="Aviso"
+              message="As condições de clima indicam um grau de risco, evite atividades nesse período!"
+            />
+          </View>
+        )}
       </View>
 
-      <WeatherIcon weather={props.weather} size={50} />
-      </View>
-      
+      {/* Divider */}
+      <View className="border-b border-gray-200 mb-5" />
 
-      <View className="px-2">
-        <Text className="text-lg font-bold">{props.sport}</Text>
-        <Text className="text-sm text-gray-500">{props.hour}</Text>
-      </View>
-      </View>
+      {/* Bottom row: WeatherIcon + info + switch */}
+      <View className="flex-row items-center justify-between gap-6 my-10">
+        <View className="flex-row">
+          <View className="flex-row gap-5">
+            <View className="bg-gray-300 rounded-full px-4 py-2">
+              <Text className="text-lg font-bold">{props.temp}°C</Text>
+            </View>
 
-      <Switch
-        trackColor={{ false: "#767577", true: "#81b0ff" }}
-        thumbColor={isEnabled ? "#81C5C5" : "#f4f3f4"}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />
-    </View>
-  </View>
+            <WeatherIcon weather={props.weather} size={50} />
+          </View>
+
+          <View className="px-2">
+            <Text className="text-lg font-bold">{props.sport}</Text>
+            <Text className="text-sm text-gray-500">{props.hour}</Text>
+          </View>
+        </View>
+
+        <Switch
+          trackColor={{ false: "#767577", true: "#81b0ff" }}
+          thumbColor={isEnabled ? "#81C5C5" : "#f4f3f4"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
+      </View>
+    </LinearGradient>
   );
 }
+
+const styles = StyleSheet.create({
+  componentCard: {
+    borderRadius: 16,
+  }
+})
